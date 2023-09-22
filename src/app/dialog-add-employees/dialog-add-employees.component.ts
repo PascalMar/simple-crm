40a67289c52a326c0cbc3c0be3d341301c45e9fd
PascalMar <pa.marienfeld@gmail.com>
2023-09-22 13:53:29 +0200

    worked on add employees

--- src/app/dialog-add-employees/dialog-add-employees.component.ts
+++ src/app/dialog-add-employees/dialog-add-employees.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatDialogRef } from '@angular/material/dialog';
+import { EmployService } from '../shared/employ.service';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-add-employees',
@@ -10,10 +11,16 @@ import { MatDialogRef } from '@angular/material/dialog';
 export class DialogAddEmployeesComponent implements OnInit {
   employeesForm!: FormGroup;
   loading = false;
+  title = 'Add Employee';
 
-  constructor(private formBuilder: FormBuilder, public dialogRef: MatDialogRef<DialogAddEmployeesComponent>) { }
+  constructor(private formBuilder: FormBuilder, public dialogRef: MatDialogRef<DialogAddEmployeesComponent>, private empService: EmployService, @Inject(MAT_DIALOG_DATA) public data: any,) { }
 
   ngOnInit(): void {
+    if (this.data !== null && this.data !== '') {
+      this.title = 'Update Employee';
+    } else {
+      this.title = 'Add Employee';
+    }
     this.employeesForm = this.formBuilder.group({
       Name: ['', Validators.required],
       Designation: ['', Validators.required],
@@ -23,11 +30,38 @@ export class DialogAddEmployeesComponent implements OnInit {
       Reportto: [''],
       City: [''],
     });
+    this.employeesForm.patchValue(this.data);
   }
 
   addEmployees() {
     if (this.employeesForm.valid) {
       const userData = this.employeesForm.value;
+      if (this.data !== null && this.data !== '') {
+        this.empService.updateEployee(this.data.id, userData).then(
+          (res: any) => {
+            this.employeesForm.reset();
+            this.dialogRef.close();
+            console.log('data is updated successfully!!');
+          }
+        ).catch(
+          (res: any) => {
+            console.log('Something went wrong!!');
+          });
+
+      } else {
+        this.empService.addEmployee(userData).then(
+          (res: any) => {
+            this.employeesForm.reset();
+            this.dialogRef.close();
+            console.log('data is added successfully!!');
+          }
+        ).catch(
+          (res: any) => {
+            console.log('Something went wrong!!');
+          });
+      }
+
+
     }
   }
 }
--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddEmployeesComponent } from '../dialog-add-employees/dialog-add-employees.component';
+import { EmployService } from '../shared/employ.service';
+import { DocumentData, QuerySnapshot } from '@firebase/firestore';
 
 @Component({
   selector: 'app-employees',
@@ -9,17 +11,65 @@ import { DialogAddEmployeesComponent } from '../dialog-add-employees/dialog-add-
 })
 export class EmployeesComponent implements OnInit {
 
+  allEmp: any;
+  empCollectiondata: any = [];
+  searchQuery:string = '';
+  filteredData: any = [];
 
-  constructor(public dialog: MatDialog) { }
+
+  constructor(public dialog: MatDialog, private empService: EmployService) { }
 
   ngOnInit(): void {
-
+    this.getEmployees();
+    this.empService.obsr_UpdatedSnapshot.subscribe((snapshot) => {
+      this.updateEmployeeCollection(snapshot);
+    })
   }
 
 
 
-  openDialog() {
-    this.dialog.open(DialogAddEmployeesComponent)
+  openDialog(item: any) {
+    this.dialog.open(DialogAddEmployeesComponent, {
+      data: item,
+    });
   }
+
+  async delete(docId: string) {
+    await this.empService.deleteEmployee(docId);
+  }
+
+  async getEmployees() {
+    const snapshot = await this.empService.getEmployees();
+    this.updateEmployeeCollection(snapshot);
+    console.log(this.empCollectiondata, ' data');
+  }
+
+  updateEmployeeCollection(snapshot: QuerySnapshot<DocumentData>) {
+    this.empCollectiondata = [];
+    snapshot.docs.forEach((emp) => {
+      this.empCollectiondata.push({ ...emp.data(), id: emp.id });
+    })
+  }
+
+  updateFilteredData() {
+    this.filteredData = this.empCollectiondata.filter((item:any) => {
+      // Customize this logic based on your search requirements
+      return (
+        item.Name.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+        item.Designation.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+        item.email.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+        item.Country.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+        item.City.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+        item.Hiredate.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+        item.Reportto.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    });
+  }
+
+  onSearch () {
+    this.updateFilteredData();
+    console.log(this.filteredData ,' filter data');
+  }
+
 
 }
