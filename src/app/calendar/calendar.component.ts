546cecb2806346f11e39876e4d59bb24203a1f51
PascalMar <pa.marienfeld@gmail.com>
2023-10-02 01:10:09 +0200

    fixed some design issues

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -81,7 +81,7 @@ export class CalendarComponent {
     {
       start: subDays(startOfDay(new Date()), 1),
       end: addDays(new Date(), 1),
-      title: 'A 3 day event',
+      title: 'Sprint with Developer Team',
       color: { ...colors['red']},
       actions: this.actions,
       allDay: true,
@@ -93,14 +93,14 @@ export class CalendarComponent {
     },
     {
       start: startOfDay(new Date()),
-      title: 'An event with no end date',
+      title: 'Daily Meeting',
       color: { ...colors['yellow']},
       actions: this.actions,
     },
     {
       start: subDays(endOfMonth(new Date()), 3),
       end: addDays(endOfMonth(new Date()), 3),
-      title: 'A long event that spans 2 months',
+      title: 'Final project phase',
       color: { ...colors['blue']},
       allDay: true,
     },
